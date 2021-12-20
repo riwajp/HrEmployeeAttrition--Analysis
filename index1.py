223ea264875059382880974cb17d5313598f1d05
riwajp <riwajfictious@gmail.com>
2021-12-20 10:16:26 +0545

    Final Commit 2.0

--- index1.py
+++ index1.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
 
-print(data['EducationField'].value_counts())
+print(data["EducationField"].value_counts())
 grp_by_field = data.groupby("EducationField")
 
-sns.kdeplot(grp_by_field.get_group('Medical')['MonthlyRate'], label='Medical')
-sns.kdeplot(grp_by_field.get_group('Marketing')
-            ['MonthlyRate'], label='Marketing')
+sns.kdeplot(grp_by_field.get_group("Medical")["MonthlyRate"], label="Medical")
+sns.kdeplot(grp_by_field.get_group("Marketing")["MonthlyRate"], label="Marketing")
+plt.xlabel(" Monthly Salary")
 plt.legend()
 plt.show()
